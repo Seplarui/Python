6c40957eb20b6f7f45392260d81215feaed0070f
Sebas <seplarui@gmail.com>
2019-12-29 13:20:07 +0100

    more functions

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,17 @@
-print("Hola Mundo")
+def mensaje():
+
+    print("Hola Mundo")
+
+
+def suma(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+
+
+
+almacena_resultado=suma(3,4)
+
+print(almacena_resultado)
+
+mensaje()
